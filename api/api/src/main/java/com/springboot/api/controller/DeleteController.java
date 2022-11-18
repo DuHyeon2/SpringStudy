813d52d7ea05c72db29f71ee8d30fa11c69e8dfc
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-18 15:38:52 +0900

    Delete Update

---
+++ api/api/src/main/java/com/springboot/api/controller/DeleteController.java
@@ -0,0 +1,19 @@
+package com.springboot.api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/delete-api")
+public class DeleteController {
+    @DeleteMapping(value = "/{variable}")
+    public String DeleteVariable(@PathVariable String variable){
+        return variable;
+    }
+
+    @DeleteMapping(value = "/request1")
+    public String DeleteVariable1(@RequestParam String email){
+        return email;
+    }
+
+
+}
