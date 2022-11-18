02120c2dd3a5f7fdcb38607da06d66b771760889
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-18 17:54:26 +0900

    Entity Update

---
+++ jpa/jpa/src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -0,0 +1,26 @@
+package com.springboot.jpa.data.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createAt;
+
+    private LocalDateTime updateAt;
+
+}
