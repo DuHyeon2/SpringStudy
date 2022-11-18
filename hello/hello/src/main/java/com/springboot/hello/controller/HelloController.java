0403a0bed77f878c1d771391ef05755f2a9dadef
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-18 13:06:09 +0900

    update

---
+++ hello/hello/src/main/java/com/springboot/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.springboot.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
