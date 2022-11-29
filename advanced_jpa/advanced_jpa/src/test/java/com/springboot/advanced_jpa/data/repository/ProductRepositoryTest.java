0e4842c2edf297c21f4eb0337aa1d30a9841d74e
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2022-11-30 00:12:07 +0900

    update

--- advanced_jpa/advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
+++ advanced_jpa/advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
@@ -1,22 +1,31 @@
 package com.springboot.advanced_jpa.data.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.springboot.advanced_jpa.data.entity.Product;
+import com.springboot.advanced_jpa.data.entity.QProduct;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
-public class ProductRepositoryTest {
+class ProductRepositoryTest {
 
     @Autowired
     ProductRepository productRepository;
 
     @Test
-    void sortingAndPagingTest(){
+    void sortingAndPagingTest() {
+        // given
         Product product1 = new Product();
         product1.setName("펜");
         product1.setPrice(1000);
@@ -34,7 +43,7 @@ public class ProductRepositoryTest {
         Product product3 = new Product();
         product3.setName("펜");
         product3.setPrice(500);
-        product3.setStock(30);
+        product3.setStock(50);
         product3.setCreateAt(LocalDateTime.now());
         product3.setUpdateAt(LocalDateTime.now());
 
@@ -42,7 +51,55 @@ public class ProductRepositoryTest {
         Product savedProduct2 = productRepository.save(product2);
         Product savedProduct3 = productRepository.save(product3);
 
-        productRepository.findByName("펜", Sort.by(Order.asc("price")));
-        productRepository.findByName("펜", Sort.by(Order.asc("price"), Order.desc("stock")));
+        System.out.println(productRepository.findByNameOrderByPriceAscStockDesc("펜"));
+
+        // 예제 8.16
+        System.out.println(productRepository.findByName("펜", Sort.by(Order.asc("price"))));
+        System.out.println(productRepository.findByName("펜", Sort.by(Order.asc("price"), Order.desc("stock"))));
+        // 예제 8.17
+        System.out.println(productRepository.findByName("펜", getSort()));
+
+//        System.out.println(productRepository.findByName("펜", PageRequest.of(0, 2)));
+//        // 예제 8.19
+//        Page<Product> productPage = productRepository.findByName("펜", PageRequest.of(0, 2));
+//        // 예제 8.20
+//        System.out.println(productPage.getContent());
+
+//        System.out.println(productRepository.findByName("펜", PageRequest.of(0, 2, Sort.by(Order.asc("price")))));
+//        System.out.println(productRepository.findByName("펜", PageRequest.of(0, 2, Sort.by(Order.asc("price")))).getContent());
+    }
+
+    // 예제 8.17
+    private Sort getSort() {
+        return Sort.by(
+                Order.asc("price"),
+                Order.desc("stock")
+        );
+    }
+    // 예제 8.26
+
+    @PersistenceContext
+    EntityManager entityManager;
+    @Test
+    void queryDslTest() {
+        JPAQuery<Product> query = new JPAQuery(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<Product> productList = query
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (Product product : productList) {
+            System.out.println("----------------");
+            System.out.println();
+            System.out.println("Product Number : " + product.getNumber());
+            System.out.println("Product Name : " + product.getName());
+            System.out.println("Product Price : " + product.getPrice());
+            System.out.println("Product Stock : " + product.getStock());
+            System.out.println();
+            System.out.println("----------------");
+        }
     }
 }
